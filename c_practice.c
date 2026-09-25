036f4cf3b2b7d68bd78a3cc124cb79947580256c
zhangzixin01 <2043087503@qq.com>
2026-09-25 19:20:49 +0800

    改为经典Hello World程序

--- c_practice.c
+++ c_practice.c
@@ -1,6 +1,6 @@
 #include <stdio.h>      // 引入"标准输入输出"库，才能用 printf
 
 int main() {            // 程序从这里开始执行
-    printf("hello.zzx\n");   // 打印一行文字；\n 是换行
+    printf("Hello, LightChaser!\n");   // 打印一行文字；\n 是换行
     return 0;           // 告诉系统"正常结束"
 }
